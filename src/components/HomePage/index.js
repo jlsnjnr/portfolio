282b7018020ba5d20d99cab3686d00ad1626c307
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-03-02 13:36:34 -0300

    photo

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,16 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./styles.module.scss";
-import mypic from "./../../../public/images/im.png";
-import {
-  AiFillGithub,
-  AiOutlineBehance,
-  AiOutlineLinkedin,
-  AiOutlineWhatsApp,
-} from "react-icons/ai";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
+import Image from 'next/image';
+import Link from 'next/link'
+import styles from './styles.module.scss';
+import mypic from './../../../public/images/im.svg'
+import { AiFillGithub, AiOutlineBehance, AiOutlineLinkedin, AiOutlineWhatsApp } from "react-icons/ai";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
 
 export function HomePage() {
   useEffect(() => {
@@ -20,6 +15,7 @@ export function HomePage() {
   return (
     <>
       <div className={styles.cardSolo}>
+
         <div className={styles.gradient}>
           <div className={styles.gradiente1}></div>
           <div className={styles.gradiente2}></div>
@@ -28,18 +24,17 @@ export function HomePage() {
 
         <div className={styles.card}>
           <div className={styles.cardContainer}>
-            <div
-              data-aos="fade-down"
-              data-aos-delay="300"
-              className={styles.cardPerson}
-            >
-              <Image
-                src={mypic}
+                
+            <div data-aos="fade-down" data-aos-delay="300" className={styles.cardPerson}>
+
+              <Image 
+                src={mypic} 
                 alt="Im"
                 className={styles.image}
-                width={120}
-                height={120}
-                unoptimized={true}
+                width={120} 
+                height={120} 
+                unoptimized={false}
+                objectFit="cover" 
               />
 
               <h2 className={styles.title}>Hi! i’m a Front-End developer 👋</h2>
@@ -48,42 +43,28 @@ export function HomePage() {
                 <a className={styles.btn}>Ver projetos</a>
               </Link>
 
+
               <div className={styles.icons}>
-                <a
-                  href="https://www.behance.net/jlsnjnr"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="https://www.behance.net/jlsnjnr" target="_blank" rel="noreferrer">
                   <AiOutlineBehance size={24} />
                 </a>
-                <a
-                  href="https://github.com/jlsnjnr"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="https://github.com/jlsnjnr" target="_blank" rel="noreferrer">
                   <AiFillGithub size={24} />
                 </a>
 
-                <a
-                  href="https://api.whatsapp.com/send?phone=5512981872315&"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="https://api.whatsapp.com/send?phone=5512981872315&" target="_blank" rel="noreferrer">
                   <AiOutlineWhatsApp size={24} />
                 </a>
 
-                <a
-                  href="https://www.linkedin.com/in/jlsnjnr/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="https://www.linkedin.com/in/jlsnjnr/" target="_blank" rel="noreferrer">
                   <AiOutlineLinkedin size={24} />
                 </a>
               </div>
+
             </div>
           </div>
-        </div>
+        </div> 
       </div>
     </>
-  );
+  )
 }
